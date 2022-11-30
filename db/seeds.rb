16c9700a6aa20f286da314eb7d5a08c0f3b585cd
unknown <senthilkumarbca>
2022-11-30 12:26:38 +0530

    seed images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create(email: "senthilthoondi@gmail.com", password: "kum32ps4") if User.count.zero?
 
-Movie.create!(name:"Love Today", release_date:"2022-11-04") if Movie.count.zero?
+movie = Movie.create(name:"Love Today", release_date:"Fri, 04 Nov 2022", average_rating: '0', reviews_count: '0')
+movie1 = Movie.create(name:"Ponniyin Selvan", release_date:"Thu, 29 Sep 2022", average_rating: '0', reviews_count: '0')
+movie2 = Movie.create(name:"Sardar", release_date:"Fri, 21 Oct 2022", average_rating: '0', reviews_count: '0')
+
+
+movie.image.attach(io: File.open("#{Rails.root}/app/assets/images/seed/lovetoday.png"), filename: "lovetoday.png", content_type: "image/png")
+movie1.image.attach(io: File.open("#{Rails.root}/app/assets/images/seed/ps1.png"), filename: "ps1.png", content_type: "image/png")
+movie2.image.attach(io: File.open("#{Rails.root}/app/assets/images/seed/sardar.png"), filename: "sardar.png", content_type: "image/png")
+
